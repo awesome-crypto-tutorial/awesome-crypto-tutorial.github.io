60394e529efd55cf9c0bf9c56bec52d654b5e78a
wanxsb@gmail.com <wanxsb@gmail.com>
2025-09-08 20:11:40 +0800

    add bot

---
+++ scripts/run-bot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load the RVM environment
+source "/etc/profile.d/rvm.sh"
+
+# Run your Ruby script
+/usr/local/rvm/rubies/ruby-3.3.0/bin/ruby /root/awesome-crypto-tutorial.github.io/scripts/social-media-bot.rb
